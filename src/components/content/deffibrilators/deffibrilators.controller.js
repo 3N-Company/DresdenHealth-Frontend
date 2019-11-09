1a414e976c60684ad0340bfd0b043dafba08bf7c
Yahor Paromau <egor.latvia@gmail.com>
2019-11-10 00:58:19 +0100

    feature-map_services :: Fix maps

--- src/components/content/deffibrilators/deffibrilators.controller.js
+++ src/components/content/deffibrilators/deffibrilators.controller.js
@@ -1,19 +1,32 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
-import MapService, {MapBuilder} from "../../../services/map/MapService";
+import {MapRenderer} from "../../../services/map/MapRenderer";
 import DeffibrilatorsService from "../../../services/deffibrilators/DeffibrilatorsService";
+import deff_img from "../../../assets/img/mark.png";
 
 class DeffibrilatorsController extends Component {
 
+    constructor(props) {
+        super(props)
+        this.id = 'deffibrilator_map'
+    }
+
     componentDidMount() {
-        (new MapBuilder())
-            .withUserPosition()
-            .withData(DeffibrilatorsService.loadJson())
+        const mapRenderer = new MapRenderer(
+            this.id,
+            DeffibrilatorsService.loadJson(),
+            deff_img
+        )
+        mapRenderer
+            .renderData()
+            .withUserPosition(mapRenderer.bindCallbacksToThis(
+                mapRenderer.renderNearest
+            ))
     }
 
     render() {
         return (
-            <div id={"map"}/>
+            <div id={this.id}/>
         );
     }
 }
---
+++ src/services/map/MapRenderer.js
@@ -0,0 +1,90 @@
+import L from "leaflet";
+import "leaflet-routing-machine"
+import {DRESDEN, TILE_LAYER_URL} from "../../constants/map.constants";
+import leafletKnn from "leaflet-knn";
+
+export class MapRenderer {
+    constructor(mapId, data, markerImg) {
+        let map = L.map(mapId).setView([DRESDEN.lat, DRESDEN.lon], 16);
+        L.tileLayer(TILE_LAYER_URL, {
+            maxZoom: 18,
+            attribution: '&copy Dresden Healthapp',
+            id: 'mapbox.light'
+        }).addTo(map);
+        this.__diffIcon = L.icon({
+            iconUrl: markerImg,
+            iconSize: [32, 37],
+            iconAnchor: [16, 37],
+            popupAnchor: [0, -28]
+        });
+        this.__map = map
+        this.__data = data
+    }
+
+    __onEachFeature(feature, layer) {
+        let popupContent = "<p>I started out as a GeoJSON " +
+            feature.geometry.type + ", but now I'm a Leaflet vector!</p>";
+
+        if (feature.properties && feature.properties.popupContent) {
+            popupContent += feature.properties.popupContent;
+        }
+
+        layer.bindPopup(popupContent);
+    }
+
+    __addUserPos(location) {
+        let userPos = new L.LatLng(location.coords.latitude, location.coords.longitude);
+        // userPos = new L.LatLng(51.029322, 13.735111);
+        this.__map.setView(userPos, 18);
+        L.circleMarker(userPos, {
+            radius: 8,
+            fillColor: "#ff7800",
+            color: "#000",
+            weight: 1,
+            opacity: 1,
+            fillOpacity: 0.8
+        }).addTo(this.__map);
+        return userPos
+    }
+
+    bindCallbacksToThis(...callbacks){
+        return callbacks.map(c => c.bind(this))
+    }
+
+    withUserPosition(callbacks) {
+        navigator.geolocation.getCurrentPosition((location) => {
+                const userPos = this.__addUserPos(location)
+                callbacks.forEach(c => c(userPos))
+            }
+        )
+    }
+
+    renderData() {
+        L.geoJSON(this.__data, {
+            pointToLayer: (feature, latlng) => L.marker(latlng, {icon: this.__diffIcon}),
+            onEachFeature: this.__onEachFeature
+        }).addTo(this.__map);
+        return this
+    }
+
+    renderNearest(userPosition) {
+        const dataLObject = L.geoJson(this.__data);
+        const sortedDataL = leafletKnn(dataLObject).nearest(L.latLng(userPosition), 16);
+        let nearestDiff = new L.LatLng(sortedDataL[0].lat, sortedDataL[0].lon);
+        L.circleMarker(nearestDiff, {
+            radius: 8,
+            fillColor: "red",
+            color: "#000",
+            weight: 1,
+            opacity: 1,
+            fillOpacity: 0.8
+        }).addTo(this.__map);
+        L.Routing.control({
+            waypoints: [
+                L.latLng(userPosition),
+                L.latLng(nearestDiff),
+            ]
+        }).addTo(this.__map);
+    }
+
+}
