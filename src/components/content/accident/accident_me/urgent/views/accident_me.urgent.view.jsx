809f02cc36a8871594e51484e648f63951ba76f7
ArtemSer <artem.sierikov@gmail.com>
2019-11-10 08:32:26 +0100

    Fixed Urgent

--- src/components/content/accident/accident_me/urgent/views/accident_me.urgent.view.jsx
+++ src/components/content/accident/accident_me/urgent/views/accident_me.urgent.view.jsx
@@ -11,7 +11,7 @@ const AccidentMeView = props => {
                     <div className="row">
                         <div className="column col-lg-10 col-sm-11 offset-1">
                             <h6>On way</h6>
-                            <h1>Ambulance is on way to you!</h1>
+                            <h1>Ambulance is on the way to you!</h1>
                         </div>
                     </div>
 
@@ -20,7 +20,7 @@ const AccidentMeView = props => {
                     <div className="row">
                         <div className="column col-lg-10 col-sm-11 offset-1">
                             <h6>Your Doctor</h6>
-                            <h1>Your doctor warned with all current info. </h1>
+                            <h1>Your doctor is informed about your current situation</h1>
                         </div>
                     </div>
 
@@ -30,7 +30,7 @@ const AccidentMeView = props => {
                         <div className="column col-lg-10 col-sm-11 offset-1">
                             <h6>Your person</h6>
 
-                            <h1>Your trust emergency contact was informed about situation </h1>
+                            <h1>Your emergency contact was informed about situation </h1>
                         </div>
                     </div>
 
@@ -39,7 +39,7 @@ const AccidentMeView = props => {
                         <div className="column col-lg-10 col-sm-11 offset-1">
                             <h6>Don't worry</h6>
 
-                            <h1>Try keep calm and wait for help. It will come as soon as possible!</h1>
+                            <h1>Try to be calm and wait for help. It will come as soon as possible!</h1>
                         </div>
                     </div>
                     <div className="gap_150"></div>
