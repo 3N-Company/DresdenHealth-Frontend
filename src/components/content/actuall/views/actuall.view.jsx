1e6cbc4c1394eba0dec0abbf158e6d82536dfee4
ArtemSer <artem.sierikov@gmail.com>
2019-11-10 04:04:02 +0100

    Added content to Actuell

--- src/components/content/actuall/views/actuall.view.jsx
+++ src/components/content/actuall/views/actuall.view.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import img_bg from '../../../../assets/img/backgrounds/actuall.jpeg'
+import img_bg from '../../../../assets/img/backgrounds/actuall.jpg'
 
 const ActuallView = props => {
     return (
@@ -18,80 +18,42 @@ const ActuallView = props => {
                 <div className="row">
                     <div className="column col-10 offset-1">
                         <div className="post_content">
-                            <h4>Street style paradise</h4>
+                            <h4>Vaccination</h4>
 
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
-                                incididunt ut
-                                labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                                ullamco
-                                laboris nisi ut aliquip ex ea commodo consequat.</p>
+                            <p>The German Vaccination Commission recommends immunisation against the following diseases: </p>
 
-                            <blockquote>
-                                But I must explain to you how all this mistaken idea of denouncing pleasure and praising
-                                pain
-                                was born.
-                            </blockquote>
-
-                            <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                                nulla
-                                pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
-                                deserunt
-                                mollit anim id est laborum.</p>
-
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor
-                                incididunt ut
-                                labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                                ullamco
-                                laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit
-                                in
-                                voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
-
-                            <div className="gap_25"/>
+                            <ul style={{listStyle: "inside"}}>
+                                <li>Tetanus</li>
+                                <li>Diphteria</li>
+                                <li>Whooping Cough (pertussis)</li>
+                                <li>Haemophilus Influenzae b (Hib)</li>
+                                <li>Polio (poliomyelitis)</li>
+                                <li>Hepatitis B</li>
+                                <li>Pneumococcus (Streptococcus pneumoniae)</li>
+                                <li>Rotavirus</li>
+                                <li>Meningococcus C (Neisseria meningitidis)</li>
+                                <li>Measles</li>
+                                <li>Mumps</li>
+                                <li>Rubella</li>
+                                <li>Chickenpox</li>
+                            </ul>
 
                             <p>
-                                <div className="gallery" data-cols="2" data-margin="20" data-ratio="auto">
-                                    <div className="entry">
-                                        <img src="assets/img/blog/single-post-01.jpg" alt="" className="responsive"/>
-                                    </div>
-                                    <div className="entry">
-                                        <img src="assets/img/blog/single-post-02.jpg" alt="" className="responsive"/>
-                                    </div>
-                                </div>
+                                For <b>elderly people</b> and <b>pregnant</b> women, the Vaccination Commission recommends inoculation against the flu (influenza).
+                                Young women are advised to get vaccinated against cervical cancer (triggered by the human papillomavirus or HPV).
                             </p>
-
-                            <div className="gap_25"/>
-
-                            <p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit
-                                anim id
-                                est laborum. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum
-                                dolore eu
-                                fugiat nulla pariatur.</p>
-
-                            <div className="gap_25"/>
-
-                            <p className="tags">
-                                <i className="fa fa-tags"/>
-                                <a href="#" className="link">Design</a>
-                                <a href="#" className="link">Tech</a>
-                                <a href="#" className="link">Tips</a>
-                            </p>
+                            <h4>Impfpass</h4>
+                            <p>
+                                When you are vaccinated for the first time in Germany, you will receive a yellow booklet
+                                which is called “vaccination record” (“Impfpass”, “Impfausweis” or “Impfbuch”). All your
+                                inoculations will be recorded in this booklet which helps you keep track of the
+                                immunisation schedule. You should bring your vaccination record along to all your
+                                medical appointments. </p>
                         </div>
                     </div>
                 </div>
             </div>
-
             <div className="gap_100"/>
-
-            <div className="project_navigation clearfix">
-                <a href="project-4.html" className="prev">
-                    <span className="meta">Prev</span>
-                    <span className="title">The best redheads of all time</span>
-                </a>
-                <a href="project-2.html" className="next">
-                    <span className="meta">Next</span>
-                    <span className="title">The purpose of your brand</span>
-                </a>
-            </div>
         </div>
     )
 }
