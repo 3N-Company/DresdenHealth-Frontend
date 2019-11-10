6c701ac95814139f3c6e4689621b09dfc584d8d9
ArtemSer <artem.sierikov@gmail.com>
2019-11-10 01:46:58 +0100

    Desing for Desktop map

--- src/components/header/views/header.view.jsx
+++ src/components/header/views/header.view.jsx
@@ -14,6 +14,7 @@ const HeaderView = props => {
                         <img src="https://www.dropbox.com/s/dff4ofuslr4dezq/hearty.svg?raw=1" alt="" className="logo_dark"/>
                         <img src="assets/img/logo-white.png" alt="" className="logo_white"/>
                     </Link>
+                    <h1 className=" mt-5">Test</h1>
                 </div>
 
                 <MenuButtonController/>
