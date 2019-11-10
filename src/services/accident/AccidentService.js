114eccd01c30d6c77a3dafa13202dcb3688482ee
Yahor Paromau <egor.latvia@gmail.com>
2019-11-10 05:36:27 +0100

    APIS

--- src/services/accident/AccidentService.js
+++ src/services/accident/AccidentService.js
@@ -1,5 +1,20 @@
-class AccidentService {
-    sendMe(){
+import axios from 'axios'
+import {BASE_URL, MESSAGE_DOCTOR, MESSAGE_FRIENDS} from "../../constants/api";
 
+class AccidentService {
+
+    message = "Help required, emergency"
+
+    sendMeFriend(){
+        return axios.post(`${BASE_URL}${MESSAGE_FRIENDS}`, {message: this.message})
+    }
+
+    sendMeDoctor(){
+        return axios.post(`${BASE_URL}${MESSAGE_DOCTOR}`, {message: this.message})
+    }
+
+    send(){
+        this.sendMeDoctor()
+        this.sendMeFriend()
     }
 }
